eb755167e68c748d287bfd7a302c4eafe14690b7
lautif005 <lautiferreria@gmail.com>
2026-02-10 00:19:23 -0300

    ...

--- src/pages/Auth/LoginScreen.jsx
+++ src/pages/Auth/LoginScreen.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Lock, Mail, ArrowRight } from 'lucide-react';
-import './loginScreen.css';
+import './LoginScreen.css';
 
 const LoginScreen = () => {
     const [email, setEmail] = useState('');
